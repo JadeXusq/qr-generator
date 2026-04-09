21eaed63a610f8cfe33f199983fa5ef68fcf3c12
shaoqiu.xu <shaoqiu.xu@lkcoffee.com>
2026-04-09 19:51:09 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/projects/generator/code/',
+  base: "/projects/generator/code/",
   build: {
-    outDir: 'docs',
-    emptyOutDir: true
+    outDir: "docs",
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
